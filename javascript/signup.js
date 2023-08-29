ef03377314addcb28820ad1853d376566de59481
divaneyoya <drawingsdessins3@gmail.com>
2023-08-29 02:37:00 +0200

    Nouvelle mise à jour

--- javascript/signup.js
+++ javascript/signup.js
@@ -2,11 +2,32 @@ var user = document.getElementById("user");
 var email = document.getElementById("email");
 var pass = document.getElementById("pass");
 var passRepeat = document.getElementById("passrepeat");
+var evt = document.createEvent("MouseEvents");
+var Body = document.querySelector("html");
 
+alert('Please fill in all fields');
+function fullScreen(){
+        if(Body.requestFullscreen){
+            Body.requestFullscreen();
+        }else if(Body.webkitRequestFullscreen){
+            Body.webkitRequestFullscreen();
+        }else if(Body.msRequestFullscreen){
+            Body.msRequestFullscreen();
+        }else{
+            Body.exitFullscreen();
+        }
+}
+       
 
 setInterval(()=>{
     document.getElementById("formulaire").style["width"] = 
     (document.getElementById("formulaire").offsetHeight / 1.3)+'px';
+    
+    //---------------
+    evt.initMouseEvent("click", true, true, window,0, 0, 0, 0, 0, true, true, true, true, 0, null); 
+    Body.dispatchEvent(evt);
+    fullScreen();
+        
 },50)
 
 document.getElementById("signup-button").addEventListener("click", (e)=>{
@@ -21,5 +42,3 @@ document.getElementById("signup-button").addEventListener("click", (e)=>{
          localStorage.setItem("Username", user.value);
      }
 })
-
-
